4f39fab33a08f6717d8a75664ba6afd064c8b7c6
Visual studio by Akulov <vspc_vs@VSPC34.RU>
2021-09-07 15:24:06 +0300

    Добавьте файлы проекта.

---
+++ 07.09.21/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07._09._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo = 0;
+            int kolichestvoPovroreniy = 0;
+            string answer;
+
+            for(int i = 0;i<10;i++)
+            {
+                chislo += 2;
+                kolichestvoPovroreniy++;
+            }
+            answer = chislo.ToString();
+            Console.WriteLine(answer);
+        }
+    }
+}
